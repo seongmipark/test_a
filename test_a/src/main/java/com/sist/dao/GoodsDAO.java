fb28d9e0f9c996bda74e9ee8e82ad8b32aad7841
pakse <pakse@DESKTOP-NPVPPCB>
2021-12-24 10:39:32 +0900

    BoardDAO, GoodsDAO 완료

---
+++ test_a/src/main/java/com/sist/dao/GoodsDAO.java
@@ -0,0 +1,10 @@
+package com.sist.dao;
+
+public class GoodsDAO {
+	public void insert() {
+		System.out.println("상품을 등록하였습니다.");
+	}
+	public void update() {
+		System.out.println("상품을 수정하였습니다.");
+	}
+}
